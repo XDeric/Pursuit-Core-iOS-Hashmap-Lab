604665068e0cddeba58b20eb81399291d71e7478
eeeric3995@yahoo.com <ihxx2s7tbu0>
2019-09-26 12:31:08 -0400

    working on it

--- DictionaryLab/DictionaryProblems.swift
+++ DictionaryLab/DictionaryProblems.swift
@@ -1,15 +1,30 @@
 import Foundation
 
 // Find the most frequently occurring character in a given string
-
 func mostFrequentlyOccurringChar(in str: String) -> Character {
-    return "a"
+    var mostFrequent: Character = " "
+    var dict = [Character:Int]()
+    
+    str.forEach { dict[$0] = (dict[$0] ?? 0) + 1}
+    if let (key, value) = dict.max(by: {$0.1 < $1.1}) {
+        print("\(key) occurs \(value) times")
+        mostFrequent = key
+        return mostFrequent
+    }
+    return mostFrequent
 }
 
 // Find the first indicies whose values sum to a given number
 
 func pairSum(arr: [Int], target: Int) -> (Int, Int) {
-    return (0, 0)
+    var dict = [Int:Int]()
+    for (key,value) in arr.enumerated(){
+        let difference = target - value
+        if let test = dict[difference] {
+            return (key, test)
+        }
+    }
+    return (0,1)
 }
 
 // Find the second most frequently occurring character in a given string
